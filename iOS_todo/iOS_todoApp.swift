c30b24188be67b0362feda6bcdbd535cbd133909
brie <24256452+gabrielacarr@users.noreply.github.com>
2021-05-13 13:38:37 -0400

    Initial Commit

---
+++ iOS_todo/iOS_todoApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_todoApp.swift
+//  iOS_todo
+//
+//  Created by Brie New on 5/13/21.
+//
+
+import SwiftUI
+
+@main
+struct iOS_todoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
